acc0128de7a66f9ea21b627dbaca987bd2f19a74
Dhiraj kumar <119129141+dhirajkumarroy@users.noreply.github.com>
2025-07-30 00:29:31 +0530

    add class and obj in java

---
+++ Java/Oops/Myname.java
@@ -0,0 +1,16 @@
+
+class Name{
+
+  String name;
+
+  int age;
+
+}
+
+public class Myname{
+  public static void main(String[] args) {
+    Name obj = new Name();
+    obj.name = "Dhiraj";
+    System.out.println(obj.name);
+  }
+}
